baa9912acbe0fefe4965a0d821207dc723d7fe6a
coji <coji@techtalk.jp>
2023-12-26 21:53:01 +0900

    Remove debug flag and update debug logic

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import './App.svelte'
 declare global {
   interface Window {
     tenantId?: string
-    debug?: boolean
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const tenantId = window.tenantId
-  const debug = window.debug === true
+  const debug = location.search.includes('debug')
+
   // Web Component として body 末尾に挿入
   document.body.insertAdjacentHTML(
     'beforeend',
-    `<test-popup tenantId="${tenantId}" debug="${debug}" />`,
+    `<test-popup tenantId="${tenantId}" ${debug ? 'debug' : ''} />`,
   )
 })
